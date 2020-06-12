a8a23e35656b486fdb7458999433eeab46bd5e1f
Shlomi Fish <shlomif@shlomifish.org>
2020-06-12 12:28:35 +0300

    add from-start

--- App-Timestamper-WithElapsed/lib/App/Timestamper/WithElapsed.pm
+++ App-Timestamper-WithElapsed/lib/App/Timestamper/WithElapsed.pm
@@ -13,6 +13,7 @@ use autodie;
 # by PEVANS (Paul Evans) and relicensed as MIT License with his
 # permission. Thanks!
 
+use Getopt::Long 2.37 qw/ GetOptionsFromArray /;
 use Time::HiRes qw/ time /;
 
 use IO::Async::Stream          ();
@@ -30,18 +31,37 @@ sub new
     return $self;
 }
 
+sub _from_start
+{
+    my $self = shift;
+
+    if (@_)
+    {
+        $self->{_from_start} = shift;
+    }
+
+    return $self->{_from_start};
+}
+
 sub _init
 {
     my ( $self, $args ) = @_;
+    my $argv = ( $args->{argv} // [] );
 
+    my $from_start;
+    GetOptionsFromArray( $argv, 'from-start!' => \$from_start, )
+        or die $!;
+    $self->_from_start($from_start);
     return;
 }
 
 sub run
 {
+    my $self = shift;
     my $loop = IO::Async::Loop->new;
 
     my $last_line_time = time();
+    my $init_time      = $self->_from_start ? ( $last_line_time + 0 ) : 0;
     STDOUT->autoflush(1);
     my $timer = IO::Async::Timer::Periodic->new(
         interval => 0.1,
@@ -53,6 +73,14 @@ sub run
             return;
         },
     );
+    my $out = sub {
+        my $t = shift;
+        my $l = shift;
+        chomp $l;
+        my $s = sprintf( "%.8f    %s", $t - $init_time, $l );
+        printf "\r%-70s\n", $s;
+        return;
+    };
 
     my $stream = IO::Async::Stream->new(
         read_handle => \*STDIN,
@@ -63,13 +91,13 @@ sub run
             {
                 my $l = $1;
                 my $t = time();
-                printf "\r%.8f\t%s", $t, $l;
+                $out->( $t, $l );
                 $last_line_time = $t;
             }
 
             if ($eof)
             {
-                printf "%.8f\t%s\n", time(), $$buffref;
+                $out->( time(), $$buffref );
             }
 
             return 0;
@@ -105,7 +133,7 @@ Constructor
 
 =head2 run
 
-Run the application.
+Run the application .
 
 =head1 CREDITS
 
