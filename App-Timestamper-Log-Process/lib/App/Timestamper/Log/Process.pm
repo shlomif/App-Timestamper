a67d6d86fbb4c179caff5f2ccc58c9d5ba81591d
Shlomi Fish <shlomif@shlomifish.org>
2024-06-08 07:27:53 +0300

    add the new Log::Process app skeleton

---
+++ App-Timestamper-Log-Process/lib/App/Timestamper/Log/Process.pm
@@ -0,0 +1,29 @@
+package App::Timestamper::Log::Process;
+
+use 5.014;
+use strict;
+use warnings;
+
+1;
+
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+App::Timestamper::Log::Process - various filters and queries for
+L<App::Timestamper> logs.
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=head2
+
+=head2
+
+=cut
+
